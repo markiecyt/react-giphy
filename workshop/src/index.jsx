646f6fc2383273117f7af72b365ce8f14cca02bb
Markie Choi <markie054@gmail.com>
2022-03-16 21:17:03 +0900

    first react workshop

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,31 @@
 import '../assets/stylesheets/application.scss';
+
+import ReactDOM from "react-dom";
+import React from "react";
+import App from './components/App';
+
+const root = document.querySelector("#root");
+
+// Funtional component
+// rsc + tab
+// const Hello = ({ name, flag }) => {
+//   return (
+//     <div>
+//       <h1>Hello {name}! {flag}</h1>
+//     </div>
+//   );
+// };
+
+// Class component
+// class Hello extends React.Component {
+//   render() {
+//     const { name, flag } = this.props;
+//     return (
+//       <div>
+//         <h1>Hello {name} {flag}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+ReactDOM.render(<App />, root);
